2fd4cf5bf4d7a143b97ea6e9243ead235e9511f9
André Vennberg <andre.vennberg@gmail.com>
2017-10-10 18:08:34 +0200

    Added timer to limit frame rate

--- Space Invaders/Main.cpp
+++ Space Invaders/Main.cpp
@@ -4,6 +4,7 @@
 
 static const int WINDOW_SIZE_X = 512;
 static const int WINDOW_SIZE_Y = 512;
+static const double WAIT_TIME = 1 / 60;
 
 void Start_Game();
 
@@ -19,13 +20,19 @@ int VGCMain(const std::vector<std::string> &arguments) {
 // Need separate scope for initialization of game so destructor can run before finalization.
 void Start_Game() {
 	Game game(WINDOW_SIZE_X, WINDOW_SIZE_Y);
+	VGCTimer timer = VGCClock::openTimer(WAIT_TIME);
 
 	while (!game.gameOver && VGCVirtualGameConsole::beginLoop()) {
+		VGCClock::reset(timer);
+
 		if (VGCDisplay::beginFrame()) {
 			game.RunFrame();
 			VGCDisplay::endFrame();
 		}
 
 		VGCVirtualGameConsole::endLoop();
+		VGCClock::wait(timer);
 	}
+
+	VGCClock::closeTimer(timer);
 }
